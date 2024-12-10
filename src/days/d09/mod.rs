5fc3cd6005f33dc7d3a7a177747a460878cb8817
z1lk <z1lk@pm.me>
2024-12-09 21:57:59 -0600

    cleanup day 9

--- src/days/d09/mod.rs
+++ src/days/d09/mod.rs
@@ -31,8 +31,9 @@ fn parse(input: &str) -> Vec<Chunk> {
 
 pub fn part_1(input: &str) -> i64 {
     let disk = parse(input);
-    let mut fragmented: Vec<Chunk> = Vec::new();
 
+    // break each chunk into one block chunks, since we need to move them independently
+    let mut fragmented: Vec<Chunk> = Vec::new();
     for chunk in &disk {
         match chunk {
             Chunk::Free { size } => {
@@ -74,83 +75,42 @@ fn frontload(mut disk: Vec<Chunk>) -> Vec<Chunk> {
         //println!("file ptr: {:?}", file_ptr);
         match disk.get(file_ptr).unwrap().clone() {
             // skip over free chunks; we don't move them.
-            Chunk::Free { size } => { file_ptr -= 1 }
-            Chunk::File { size, id } => {
+            Chunk::Free { size: _ } => { file_ptr -= 1 }
+            Chunk::File { size: file_size, id: file_id } => {
                 let mut free_ptr = earliest_free;
                 let mut found_new_earliest_free = false;
                 while free_ptr < file_ptr {
-                    if let Chunk::Free { size: size2 } = disk.get(free_ptr).unwrap() {
+                    if let Chunk::Free { size: free_size } = disk.get(free_ptr).unwrap() {
                         // track the earliest free chunk as we are looking for space for files,
                         // so we don't have to start from the beginning each time.
                         if !found_new_earliest_free {
-                            if free_ptr > earliest_free {
-                                earliest_free = free_ptr;
-                            }
+                            if free_ptr > earliest_free { earliest_free = free_ptr }
                             found_new_earliest_free = true;
                         }
-                        if *size2 >= size {
-                            let mut replace_with = vec![Chunk::File { size: size, id: id }];
-                            let remaining = size2 - size;
+                        if *free_size >= file_size {
+                            let mut replace_with = vec![Chunk::File { size: file_size, id: file_id }];
+                            let remaining = free_size - file_size;
                             // for part 2. if the free space is larger than the file, fill
                             // the remaining free space with a new Free chunk.
                             if remaining > 0 {
                                 replace_with.push(Chunk::Free { size: remaining });
-                                file_ptr += 1;
+                                file_ptr += 1; // this extra Free chunk will shift remaining chunks up 
                             }
                             disk.splice(free_ptr..(free_ptr+1), replace_with);
                             disk.remove(file_ptr);
                             // replace the moved file with free space in case some later files
                             // weren't moved, because file position matters for the final checksum
-                            disk.insert(file_ptr, Chunk::Free { size });
-                            // if we added an extra Free in addition to the File, inc free_ptr so
-                            // that when it is decremented below, it will effectively stay where it
-                            // is, yet reference the next element since everything been shifted up.
-                            if remaining > 0 {
-                                free_ptr += 1;
-                            }
-                            // doesn't seem to affect solution speed
-                            /* if empty_beyond(&disk, file_ptr) {
-                                println!("trimming disk @ {:?}", file_ptr);
-                                disk.drain(file_ptr..);
-                                print(&disk);
-                            } */
+                            disk.insert(file_ptr, Chunk::Free { size: file_size });
                             break;
                         }
                     }
                     free_ptr += 1;
                 }
                 file_ptr -= 1;
-
             }
         }
     }
     disk
-}
-
-fn empty_beyond(disk: &Vec<Chunk>, index: usize) -> bool {
-    for chunk in &disk[index..] {
-        match chunk {
-            Chunk::Free { size } => (),
-            Chunk::File { size, id } => { return false }
-        }
-    }
-    true
-}
-
-fn print(disk: &Vec<Chunk>) {
-    println!("");
-    for chunk in disk {
-        match chunk {
-            Chunk::Free { size } => {
-                for _ in (0..*size) { print!(". ") }
-            }
-            Chunk::File { size, id } => {
-                for _ in (0..*size) { print!("{:?} ", id) }
-            }
-        }
-    }
-    println!("");
-    println!("");
 }
 
 fn checksum(disk: Vec<Chunk>) -> i64 {
@@ -172,6 +132,38 @@ fn checksum(disk: Vec<Chunk>) -> i64 {
     }
     checksum
 }
+
+fn print(disk: &Vec<Chunk>) {
+    println!("");
+    for chunk in disk {
+        match chunk {
+            Chunk::Free { size } => {
+                for _ in (0..*size) { print!(". ") }
+            }
+            Chunk::File { size, id } => {
+                for _ in (0..*size) { print!("{:?} ", id) }
+            }
+        }
+    }
+    println!("");
+    println!("");
+}
+
+// doesn't seem to affect solution speed
+/* if empty_beyond(&disk, file_ptr) {
+    println!("trimming disk @ {:?}", file_ptr);
+    disk.drain(file_ptr..);
+    print(&disk);
+}
+fn empty_beyond(disk: &Vec<Chunk>, index: usize) -> bool {
+    for chunk in &disk[index..] {
+        match chunk {
+            Chunk::Free { size } => (),
+            Chunk::File { size, id } => { return false }
+        }
+    }
+    true
+}*/
 
 #[cfg(test)]
 mod tests {
