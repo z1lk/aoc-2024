e8f3b6f174fbbfc5ca71330fc43953803b18ca49
z1lk <z1lk@pm.me>
2024-12-21 15:37:23 -0600

    day 21 part 2

--- src/days/d21/mod.rs
+++ src/days/d21/mod.rs
@@ -1,5 +1,5 @@
 use crate::grid::Grid;
-//use std::collections::HashMap;
+use std::collections::HashMap;
 use std::{thread, time};
 
 pub mod inputs {
@@ -11,97 +11,73 @@ fn parse(input: &str) -> Vec<String> {
     crate::helpers::to_lines(input)
 }
 
-pub fn part_1(input: &str) -> i32 {
+/*
+pub fn part_1_original(input: &str) -> i32 {
     let codes = parse(input);
-    //println!("{:?}",codes);
-    let numpad = Grid::from_str("789\n456\n123\n#0A");
-    //numpad.draw(false);
-    let keypad = Grid::from_str("#^A\n<v>");
-    //keypad.draw(false);
 
-    let mut r1 = Robot { pad: numpad.clone(), cur: 'A' };
-    let mut r2 = Robot { pad: keypad.clone(), cur: 'A' };
-    let mut r3 = Robot { pad: keypad.clone(), cur: 'A' };
-
-    let sleep = 0;
+    let mut robot = Robot::new(2);
 
     let mut total_complexity = 0;
 
-    //let code = codes.get(2).unwrap();
     for code in codes {
-        let mut r1_keys: Vec<char> = Vec::new();
-        let mut r2_keys: Vec<char> = Vec::new();
-        let mut r3_keys: Vec<char> = Vec::new();
-        let mut my_keys: Vec<char> = Vec::new();
+        for c in code.chars() {
+            robot.press(c);
+        }
 
         let num = code.get(0..3).unwrap().parse::<i32>().unwrap();
+        let presses = robot.get_presses();
 
-        //println!("==== {}", code);
-        thread::sleep(time::Duration::from_millis(sleep));
-
-        // for numeric code
-        for c in code.chars() {
-            // dirs that r1 needs to move to press it
-            let dirs2 = get_dirs(&r1.pad, r1.cur, c);
-            r1.cur = c;
-            // chars that r2 needs to press to move r1
-            let mut chars2: Vec<char> = dirs2.into_iter().map(|d| dir_to_char(d)).collect();
-            // r2 needs to press A afterwards
-            chars2.push('A');
-            r1_keys.push(c);
-            //println!("=== {}", c);
-            //println!("chars2: {:?}",chars2);
-            thread::sleep(time::Duration::from_millis(sleep));
-            for c2 in chars2 {
-                // dirs that r2 needs to move to press it
-                let mut dirs3 = get_dirs(&r2.pad, r2.cur, c2);
-                r2.cur = c2;
-                // chars that r3 needs to press to move r2
-                let mut chars3: Vec<char> = dirs3.into_iter().map(|d| dir_to_char(d)).collect();
-                // r3 needs to press A afterwards
-                chars3.push('A');
-                r2_keys.push(c2);
-                //println!("== {}", c2);
-                //println!("chars3: {:?}",chars3);
-                thread::sleep(time::Duration::from_millis(sleep));
-                for c3 in chars3 {
-                    // dirs that r3 needs to move to press it
-                    let mut dirs4 = get_dirs(&r3.pad, r3.cur, c3);
-                    r3.cur = c3;
-                    // chars that I need to press to move r3
-                    let mut chars4: Vec<char> = dirs4.into_iter().map(|d| dir_to_char(d)).collect();
-                    // I need to press A afterwards
-                    chars4.push('A');
-                    r3_keys.push(c3);
-                    //println!("= {}", c3);
-                    //println!("chars4: {:?}",chars4);
-                    thread::sleep(time::Duration::from_millis(sleep));
-                    for c4 in chars4 {
-                        my_keys.push(c4);
-                        //print!("{}", c4);
-                    }
-                    //println!("");
-                }
-            }
-        }
-        //println!("---");
-        //println!("{:?}", my_keys.len());
-        //print_keys(&my_keys);
-        //println!("{:?}", num);
-        let complexity = num * (my_keys.len() as i32);
+        let complexity = num * presses.len() as i32;
         total_complexity += complexity;
-        //print_keys(&r3_keys);
-        //print_keys(&r2_keys);
-        //print_keys(&r1_keys);
+        robot.clear_presses();
+    }
+
+    total_complexity
+}
+*/
+
+pub fn part_1(input: &str) -> i64 {
+    let codes = parse(input);
+    let mut robot = Robot::new(4);
+    let mut total_complexity = 0;
+
+    for code in codes {
+        let mut presses = 0;
+        for c in code.chars() {
+            presses += robot.press_and_get(c);
+        }
+        let num = code.get(0..3).unwrap().parse::<i64>().unwrap();
+        let complexity = num * presses;
+        total_complexity += complexity;
     }
 
     total_complexity
 }
 
+pub fn part_2(input: &str) -> i64 {
+    let codes = parse(input);
+    let mut robot = Robot::new(27);
+    let mut total_complexity = 0;
+
+    for code in codes {
+        let mut presses = 0;
+        for c in code.chars() {
+            presses += robot.press_and_get(c);
+        }
+        let num = code.get(0..3).unwrap().parse::<i64>().unwrap();
+        let complexity = num * presses;
+        total_complexity += complexity;
+    }
+
+    total_complexity
+}
+
+/*
 fn print_keys(keys: &Vec<char>) {
     let string: String = keys.into_iter().collect();
     println!("{}", string);
 }
+*/
 
 fn dir_to_char(dir: Dir) -> char {
     match dir {
@@ -113,8 +89,125 @@ fn dir_to_char(dir: Dir) -> char {
 }
 
 struct Robot {
+    id: i32,
     pad: Grid<char>,
-    cur: char
+    cur: char,
+    presses: Vec<char>,
+    presses_cache: HashMap::<String, i64>,
+    controller: Option<Box<Robot>>
+}
+
+impl Robot {
+    pub fn new(chain_len: i32) -> Self {
+        let id = 1;
+        let numpad = Grid::from_str("789\n456\n123\n#0A");
+        let mut r = Robot {
+            id,
+            pad: numpad,
+            cur: 'A',
+            presses: Vec::<char>::new(),
+            presses_cache: HashMap::new(),
+            controller: None
+        };
+        if id < chain_len {
+            let controller = Robot::new_controller(id + 1, chain_len);
+            r.controller = Some(Box::new(controller));
+        }
+        r
+    }
+
+    pub fn new_controller(id: i32, chain_len: i32) -> Self {
+        let keypad = Grid::from_str("#^A\n<v>");
+        let mut r = Robot {
+            id,
+            pad: keypad,
+            cur: 'A',
+            presses: Vec::<char>::new(),
+            presses_cache: HashMap::new(),
+            controller: None
+        };
+        if id < chain_len {
+            let controller = Robot::new_controller(id + 1, chain_len);
+            r.controller = Some(Box::new(controller));
+        }
+        r
+    }
+
+    // This is for the original solution for part 1.
+    // It does not do any caching, and stores all keypresses
+    // which can be later be grabbed and reset.
+    /*
+    fn get_presses(&mut self) -> Vec<char> {
+        match &mut self.controller {
+            None => return self.presses.clone(),
+            Some(r) => {
+                return r.get_presses();
+            }
+        }
+    }
+
+    fn clear_presses(&mut self) {
+        self.presses = Vec::new();
+        match &mut self.controller {
+            None => (),
+            Some(r) => { r.clear_presses() }
+        }
+    }
+
+    fn press(&mut self, c: char) {
+        self.presses.push(c);
+        // dirs self needs to move to push c
+        let dirs = get_dirs(&self.pad, self.cur, c);
+        self.cur = c;
+        // chars that controller needs to press to move self
+        let mut chars: Vec<char> = dirs.into_iter().map(|d| dir_to_char(d)).collect();
+        // controller needs to press A afterwards
+        chars.push('A');
+        match &mut self.controller {
+            None => (),
+            Some(r) => {
+                for c2 in chars {
+                    r.press(c2);
+                }
+            }
+        }
+    }
+    */
+
+    fn press_and_get(&mut self, c: char) -> i64 {
+        let cache_key = format!("{}:{}", self.cur, c);
+        if let Some(v) = self.presses_cache.get(&cache_key) {
+            // Very important that we set the current key as if we moved,
+            // even though we are returning a cached value. The cost of future
+            // presses depends on which key we are starting from.
+            self.cur = c;
+            return *v
+        }
+
+        self.presses.push(c);
+        // dirs self needs to move to push c
+        let dirs = get_dirs(&self.pad, self.cur, c);
+        self.cur = c;
+        // chars that controller needs to press to move self
+        let mut chars: Vec<char> = dirs.into_iter().map(|d| dir_to_char(d)).collect();
+        // controller needs to press A afterwards
+        chars.push('A');
+        let num = match &mut self.controller {
+            None => 1,
+            Some(r) => {
+                let mut sum = 0_i64;
+                for c2 in chars {
+                    let x = r.press_and_get(c2);
+                    sum += x;
+                }
+                sum
+            }
+        };
+
+        self.presses_cache.insert(cache_key.clone(), num.into());
+
+        num
+    }
 }
 
 #[derive(Clone,Debug)]
@@ -126,6 +219,7 @@ struct Node {
     offset: Option<(i32, i32)>
 }
 
+// get a list of the directions to move from key `start` to key `end` on keypad `grid`
 fn get_dirs(grid: &Grid<char>, start: char, end: char) -> Vec<Dir> {
     let mut dirs = Vec::<Dir>::new();
 
@@ -147,14 +241,12 @@ fn get_dirs(grid: &Grid<char>, start: char, end: char) -> Vec<Dir> {
     if dx < 0 { left = dx.abs() }
     if dx > 0 { right = dx }
 
-    // We need to order the dirs in a certain way to minimize key presses of parent,
+    // We need to order the dirs to minimize key presses of parent,
     // but we also can't make the robot move over the empty spot.
 
-    // check what the cell upwards is
-    
     let mut done = false;
 
-    // if U/D is the empty spot, so go L/R first
+    // if U/D is the empty spot, go L/R first
     let c = grid.get(x1, y1+dy).unwrap();
     if c == '#' {
         done = true;
@@ -164,7 +256,7 @@ fn get_dirs(grid: &Grid<char>, start: char, end: char) -> Vec<Dir> {
         for _ in 0..down { dirs.push(Dir::D) }
     }
 
-    // if L/R is the empty spot, so go U/D first
+    // if L/R is the empty spot, go U/D first
     if !done {
         let c = grid.get(x1+dx, y1).unwrap();
         if c == '#' {
@@ -177,7 +269,15 @@ fn get_dirs(grid: &Grid<char>, start: char, end: char) -> Vec<Dir> {
     }
 
     if !done {
-        // neither axis has the empty spot, prioritize this way
+        // Neither move hits the empty spot.
+        // Going L is expensive because it is the left-most key from A,
+        // where we always return. So we want to get L presses out of
+        // the way at the beginning, and then remaining presses
+        // will require R/U/D which are cheaper.
+        //
+        // This is hard to get an intuitive sense of, but you can prove it by thinking
+        // about the moves required in a chain length greater than 3.
+        // https://www.reddit.com/r/adventofcode/comments/1hj7f89/comment/m34erhg
         for _ in 0..left { dirs.push(Dir::L) }
         for _ in 0..up { dirs.push(Dir::U) }
         for _ in 0..down { dirs.push(Dir::D) }
@@ -200,32 +300,27 @@ fn offset_to_dir(dx: i32, dy: i32) -> Dir {
     }
 }
 
-pub fn part_2(input: &str) -> i32 {
-    let parsed = parse(input);
-    0
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn part_1_sample() {
-        assert_eq!(part_1(inputs::SAMPLE), 0);
+        assert_eq!(part_1(inputs::SAMPLE), 126384);
     }
 
     #[test]
     fn part_1_real() {
-        assert_eq!(part_1(inputs::REAL), 0);
+        assert_eq!(part_1(inputs::REAL), 184180);
     }
 
     #[test]
     fn part_2_sample() {
-        assert_eq!(part_2(inputs::SAMPLE), 0);
+        assert_eq!(part_2(inputs::SAMPLE), 154115708116294);
     }
 
     #[test]
     fn part_2_real() {
-        assert_eq!(part_2(inputs::REAL), 0);
+        assert_eq!(part_2(inputs::REAL), 231309103124520);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ use days::d21 as day;
 fn main() {
     println!("{}", day::part_1(day::inputs::SAMPLE));
     println!("{}", day::part_1(day::inputs::REAL));
-    //println!("{}", day::part_2(day::inputs::SAMPLE));
-    //println!("{}", day::part_2(day::inputs::REAL));
+    println!("{}", day::part_2(day::inputs::SAMPLE));
+    println!("{}", day::part_2(day::inputs::REAL));
 }
